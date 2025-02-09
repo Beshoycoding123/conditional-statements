7a410c6778ee5a7b5ab5d7b6383d9b9e6bfcdfab
beshoycoding123 <beshoy.shawky.2013@gmail.com>
2025-02-09 12:22:16 +0400

    if statment

---
+++ ifstatements.py
@@ -0,0 +1,8 @@
+num=9
+if num>0:
+    print(num,"is a positive number")
+number=int(input("enter a number: "))
+if number>0:
+    print(number,"is a positive number")
+else:
+    print(number,"is a negative number")
